84de73f641d0539cd76c4e790b1a7d478af3dcdb
Solvek <sa@mobiwolf.com>
2018-04-26 11:08:16 +0300

    Possibility to set datetime via spi

--- gmonitor/gmonitor.ino
+++ gmonitor/gmonitor.ino
@@ -1,7 +1,7 @@
-/* How to use the DHT-22 sensor with Arduino uno
-   Temperature and humidity sensor
-   More info: http://www.ardumotive.com/how-to-use-dht-22-sensor-en.html
-   Dev: Michalis Vasilakis // Date: 1/7/2015 // www.ardumotive.com */
+/* 
+ *  To set datetime execute command "#TYYMMDDHHmmSS"
+ *  For example "#T1804261107"
+*/
 
 //Libraries
 #include <DHT.h>;
@@ -18,6 +18,7 @@
 #define DHTTYPE DHT22   // DHT 22  (AM2302)
 DHT dht(DHTPIN, DHTTYPE); //// Initialize DHT sensor for normal 16mhz Arduino
 RTClib RTC;
+DS3231 Clock;
 
 //Variables
 int chk;
@@ -40,10 +41,14 @@ void loop()
     Serial.println(padZero(now.minute()));
     delay(DELAY_TIME);
 
+    readCommand();
+
     Serial.print(padZero(now.day()));
     Serial.print(".");
     Serial.println(padZero(now.month()));
     delay(DELAY_DATE);
+
+    readCommand();
   
     //Read data and store it to variables hum and temp
     hum = dht.readHumidity();
@@ -56,6 +61,56 @@ void loop()
     delay(DELAY_TEMP);
 
     Serial.println();
+
+    readCommand();
+}
+
+void readCommand(){
+//  Serial.println("Checking command");
+  int r;
+  do{   
+    r = Serial.read();
+    if (r == -1) return;
+//    Serial.print("Read symbol: ");
+//    Serial.println(r);
+  }while(r != 35); // # char
+
+//  Serial.println("Expecting command");
+  r = inputSymbol();
+  
+  if (r == 84 || r == 116){
+    commandSetTime();
+  }
+  else {
+    Serial.print("Unknown command: ");
+    Serial.println(r);
+  }
+}
+
+// Datetime in format YYMMDDHHmmSS
+void commandSetTime(){
+//  Serial.println("Setting time");
+  Clock.setYear(readNumeral(2));
+  Clock.setMonth(readNumeral(2));
+  Clock.setDate(readNumeral(2));
+  Clock.setHour(readNumeral(2));
+  Clock.setMinute(readNumeral(2));
+  Clock.setSecond(readNumeral(2));
+}
+
+int readNumeral(int count){
+  int r = 0;
+  for(int i=0;i<count;i++){
+    r *= 10;
+    r += inputSymbol()-48;
+  }
+  return r;
+}
+
+int inputSymbol(){
+  int s;
+  while((s=Serial.read()) == -1);
+  return s;
 }
 
 String padZero(int val){
